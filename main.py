0a87a55da15e95faf3c26cd56b767e138aa0cb09
chaplyk <bogdan.chaplyk@gmail.com>
2019-08-07 10:48:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ def calculation():
     if request.method=='POST':
         first = request.form['first']
         second = request.form['second']
+        action = request.form['action']
         if first and second:
             try:
                 for x in first + second:
                     if x != "0" and x != "1":
                         raise ValueError
-                       
+                if action == "+":       
                     result = bin(int(first, 2) + int(second, 2))[2:]
+                elif action == "-": 
+                    result = bin(int(first, 2) - int(second, 2))[2:]
             except ValueError:
                 error = 'Please use binary input only.'
     return render_template('main.html', result=result, error=error)
